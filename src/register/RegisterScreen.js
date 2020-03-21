96cdb5dafe1081bf96d5c5c4345c7388260cf581
Vlad Marusca <vlad@redbeesoftware.com>
2020-03-21 14:18:24 +0200

    Created empty cards

--- src/register/RegisterScreen.js
+++ src/register/RegisterScreen.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import Carousel from 'react-native-snap-carousel';
+import {View} from 'react-native';
 import {registerScreenStyles} from './styles';
+import {metrics} from '../themes';
 
 const RegisterScreen = () => {
-  return <View style={registerScreenStyles.container} />;
+  const carouselRef = useRef(null);
+  const cards = [
+    {id: 0, data: 'card 1'},
+    {id: 1, data: 'card 2'},
+    {id: 2, data: 'card 3'},
+    {id: 3, data: 'card 4'},
+  ];
+
+  const renderItem = useCallback(({item, index}) => {
+    console.log("Index:", index)
+    switch (index) {
+      case 0:
+        return <View style={registerScreenStyles.firstCard} />;
+      case 1:
+        return <View style={registerScreenStyles.secondCard} />;
+
+      default:
+        return <View style={registerScreenStyles.firstCard} />;
+    }
+  }, []);
+
+  return (
+    <Carousel
+      useScrollView
+      keyboardDismissMode="on-drag"
+      keyboardShouldPersistTaps="handled"
+      ref={carouselRef}
+      data={cards}
+      renderItem={renderItem}
+      sliderWidth={metrics.screenWidth}
+      itemWidth={metrics.cardWidth}
+      inactiveSlideOpacity={0.85}
+      inactiveSlideScale={0.93}
+      swipeThreshold={metrics.screenWidth * 0.1}
+    />
+  );
 };
 
 export default RegisterScreen;
--- src/register/styles/registerScreenStyles.js
+++ src/register/styles/registerScreenStyles.js
@@ -1,10 +1,23 @@
 import {StyleSheet} from 'react-native';
-import {colors} from '../../themes';
+import {colors, metrics} from '../../themes';
+import DeviceInfo from 'react-native-device-info'
 
 const registerScreenStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  firstCard: {
+    backgroundColor: colors.white,
+    height: metrics.screenHeight - metrics.size300,
+    borderRadius: metrics.size6,
+    marginTop: DeviceInfo.hasNotch() ? metrics.size93 : metrics.size73,
+  },
+  secondCard: {
+    backgroundColor: colors.black,
+    height: metrics.screenHeight - metrics.size300,
+    borderRadius: metrics.size6,
+    marginTop: DeviceInfo.hasNotch() ? metrics.size93 : metrics.size73,
   },
 });
 
