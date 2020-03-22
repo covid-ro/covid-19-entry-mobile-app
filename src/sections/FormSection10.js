e61741c4632935e0d27e8b2e5d506f85ae079254
Motoc Marius - Constantin <marius.motoc@redbeesoftware.com>
2020-03-22 16:33:27 +0200

    Code review issue fixed

--- src/sections/FormSection10.js
+++ src/sections/FormSection10.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import {InputField} from '../core/components';
 import {formSection10Styles} from './styles';
 import {strings} from '../core/strings';
 
 const FormSection10 = () => {
+  const [location, setLocation] = useState('');
   return (
     <View>
       <Text style={formSection10Styles.textContainer}>
         {strings.form10Label}
       </Text>
 
-      <InputField placeholder={strings.form10Placeholder} />
+      <InputField
+        placeholder={strings.form10Placeholder}
+        value={location}
+        onChangeText={setLocation}
+      />
     </View>
   );
 };
