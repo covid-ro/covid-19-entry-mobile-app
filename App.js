01ff79f8eefd5df4a08c7f297374d8ff849fc8cc
Iulia Santa <iulia.santa@redbeesoftware.com>
2020-03-26 15:42:09 +0200

    splash screen in android

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import {StatusBar} from 'react-native';
 import {MainNavigator} from './src/navigation';
 
-const App = () => (
-  <View style={{flex: 1}}>
-    <StatusBar barStyle="dark-content" />
-    <MainNavigator />
-  </View>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar barStyle="dark-content" />
+      <MainNavigator />
+    </View>
+  );
+};
 
 export default App;
