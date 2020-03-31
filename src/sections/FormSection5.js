70a164b82de717de012878e2f7ddab8a404f8e50
AndreiStoica32 <andrei@foodstyles.com>
2020-03-31 19:07:46 +0300

    Fixed functionality for add another user to SelectionButtons

--- src/sections/FormSection5.js
+++ src/sections/FormSection5.js
@@ -11,7 +11,6 @@ import {connect} from 'react-redux';
 const FormSection5 = ({email, phoneNumber, setEmail}) => {
   const [locationValue, setLocationValue] = useState(undefined);
   const emailRef = useRef(null);
-  console.log(phoneNumber);
   return (
     <View style={formSection5Styles.container}>
       <Text style={[labelStyles.textStyle, formSection5Styles.topTextStyle]}>
